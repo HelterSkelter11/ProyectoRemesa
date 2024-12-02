afd0a29a370781e0f5e49e5b5e8bd0cec9fbc35a
ColdCrawd <edgar.r.0228@unitec.edu>
2024-12-02 11:02:03 -0600

    Esooooo

--- lib/apis/user_api.dart
+++ lib/apis/user_api.dart
@@ -62,21 +62,70 @@ class UserApi {
     return null;
   }
 
-  Future<List<dynamic>> obtenerTransacciones(String userId) async {
-    final response = await supabase
-        .from('historia')
-        .select('transacciones(*)')
-        .eq('user_id', userId)
-        .count();
+  Future<void> agregarTransaccion({ required String direccionDestino, required double monto, required String stablecoin, required String descripcion, required bool entrada}) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final userId = prefs.getInt('user_id');
 
-    if (response.data.isEmpty == 0) {
-      throw Exception('Error al obtener transacciones: $response');
+      if (userId != null) {
+        final direccionUser = await supabase
+            .from('direcciones')
+            .select('direccion')
+            .eq('user_id', userId)
+            .maybeSingle();
+
+        if (direccionUser != null && direccionUser['direccion'] != null) {
+          final direccionSalida = direccionUser['direccion'];
+
+          final balanceResponse = await supabase
+              .from('usuarios')
+              .select('balance')
+              .eq('user_id', userId)
+              .single();
+
+          final balance = balanceResponse['balance'];
+
+          if (monto > balance) {
+            throw Exception('Saldo insuficiente');
+          }
+
+          final response = await supabase.from('transaccion').insert({
+            'descripcion': descripcion,
+            'monto': monto,
+            'stablecoin': stablecoin,
+            'estado': 'pendiente',
+            'direccion_salida': direccionSalida,
+            'direccion_entrada': direccionDestino,
+            'metodo_conversion': 'manual', //no se que poner aqui xd
+          }).select();
+
+          if (response.isEmpty) {
+            throw Exception('Error al crear la transacción: ${response}');
+          }
+
+          final transaccionId = response[0]['id'];
+          await supabase.from('historial').insert([
+            {
+              'transaccion_id': transaccionId,
+              'direccion_salida': direccionSalida,
+              'direccion_entrada': direccionDestino,
+              'hecho_en': DateTime.now().toIso8601String(),
+            }
+          ]);
+
+          print('Transacción creada con éxito');
+        } else {
+          throw Exception('No se encontró la dirección del usuario');
+        }
+      } else {
+        throw Exception('Usuario no ha iniciado sesión');
+      }
+    } catch (e) {
+      print('Error al agregar transacción: $e');
     }
-
-    return response.data;
   }
 
-  Future<void> agregarTransaccion({
+  /*Future<void> agregarTransaccion({
     required bool entrada,
     required String direccion,
     required String descripcion,
@@ -101,5 +150,5 @@ class UserApi {
     }
 
     print('Transacción agregada: ${response.data}');
-  }
+  }*/
 }
